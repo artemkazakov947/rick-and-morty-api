e851ab02fe479687c1f3faaea28f1ea5ccf85cce
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-17 14:08:43 +0300

    Added scraper functionality to the project

--- characters/models.py
+++ characters/models.py
@@ -21,5 +21,5 @@ class Character(models.Model):
     gender = models.CharField(max_length=50, choices=GenderChoices.choices)
     image = models.URLField(max_length=255, unique=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
---
+++ characters/scraper.py
@@ -0,0 +1,35 @@
+import requests
+
+from characters.models import Character
+from rick_and_morty_api.settings import RICK_AND_MORTY_CHARACTER_URL
+
+
+def scrape_character() -> list[Character]:
+    next_character_to_scrape = RICK_AND_MORTY_CHARACTER_URL
+
+    characters = []
+
+    while next_character_to_scrape is not None:
+        character_resp = requests.get(next_character_to_scrape).json()
+
+        for character in character_resp["results"]:
+            characters.append(Character(
+                name=character["name"],
+                api_id=character["id"],
+                status=character["status"],
+                species=character["species"],
+                gender=character["gender"],
+                image=character["image"]
+            ))
+        next_character_to_scrape = character_resp["info"]["next"]
+    return characters
+
+
+def save_characters(characters: list[Character]) -> None:
+    for character in characters:
+        character.save()
+
+
+def sync_characters_with_api() -> None:
+    characters = scrape_character()
+    save_characters(characters)
